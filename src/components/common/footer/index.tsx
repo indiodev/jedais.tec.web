9ab654cdcf692c844504aef883ae652be2e34dd6
jhollyfer <jhollyfer.fr@gmail.com>
2024-04-30 21:38:34 -0500

    fix: home

--- src/components/common/footer/index.tsx
+++ src/components/common/footer/index.tsx
@@ -4,9 +4,19 @@ export function Footer(): React.ReactElement {
 	return (
 		<footer className="py-12 bg-slate-950 mt-36">
 			<div className="container flex flex-col gap-4 justify-center items-center">
-				<p className="text-center text-white">
+				<p className="text-center text-white text-xl">
 					Todos os direitos reservados &copy; Jedais Tec
 				</p>
+
+				<span className="opacity-20 text-base">
+					Desenvolvido por{' '}
+					<a
+						href="https://indio.dev.br"
+						className="cursor-pointer font-bold"
+					>
+						Jhollyfer Development
+					</a>
+				</span>
 
 				<ul className="flex gap-4">
 					<li className="cursor-pointer">
